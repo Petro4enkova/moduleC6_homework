f4d72495b9bfd11bfe27c62343a4277d0a5dbd0b
Petro4enkova <121767619+Petro4enkova@users.noreply.github.com>
2023-05-09 17:58:49 +0300

    Add files via upload

---
+++ task_2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const btn = document.querySelector('.btn');
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+
+
+
+btn.addEventListener('click', () => {
+    alert('Размер экранаи ' + screenWidth + ' x ' + screenHeight)
+})
